4f68635de65d23895668897bc176752191dcce84
Yura Miyazawa <y20051205@icloud.com>
2026-07-31 00:29:07 +0900

    fix: GitHub Pagesのbaseを設定

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/my-website/',
+
   build: {
     outDir: 'docs',
-    
+
     rollupOptions: {
       input: {
         main: resolve(import.meta.dirname, 'index.html'),
